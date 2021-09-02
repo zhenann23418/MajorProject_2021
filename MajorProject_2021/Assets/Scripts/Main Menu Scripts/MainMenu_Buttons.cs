235498aef4396d1986342ead408acd46f3c154d1
zhenann23418 <74691114+zhenann23418@users.noreply.github.com>
2021-09-02 22:39:59 +0800

    63rd Commit
    - Created a new scene for the main menu which will allow the user to pick whichever topic they would like to start with first
    - Started designing the UI for the main menu
    - Imported a background for the main menu and attached it to the panel
    - Downloaded and Imported a button UI asset pack from the Unity Asset Store to add some designs to the button rather than a plain design
    - Completed the UI for the main menu scene
    - Started scripting for the buttons to have some functionality, allowing them to either proceed to the respective scenes or to quit the application
    - Completed the code for all the buttons

---
+++ MajorProject_2021/Assets/Scripts/Main Menu Scripts/MainMenu_Buttons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//In order to change scenes, you need to use the line below: 
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Buttons : MonoBehaviour
+{
+    //for the CPR Button -> leads to the CPR scene to play the animations
+    public void PlayCPR()
+    {
+        SceneManager.LoadScene("CPR Animation Scene");
+    }
+
+    //For the Grease Fire Button -> leads to the Grease Fire scene to play the animations
+    public void PlayGreaseFire()
+    {
+        SceneManager.LoadScene("Grease Fire Animation Scene");
+    }
+
+    //For the exit button -> basically allows the user to quit the program
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
